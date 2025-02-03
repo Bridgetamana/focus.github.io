3dd4d8808ca4f15231dacbf845c3121efa5191b3
Bridget Amana <amanabridget03@gmail.com>
2025-02-03 19:45:05 +0100

    feat: update HTML structure and improve accessibility

--- src/script.js
+++ src/script.js
@@ -169,10 +169,14 @@ class PomodoroTimer {
                         data-task-id="${this.currentTask.id}"
                     >${this.currentTask.name}</span>
                     <button class="complete-btn" onclick="window.pomodoroTimer. markTaskComplete(${this.currentTask.id})">
-                        <i class="fas fa-check"></i>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="20" height="20">
+                            <path fill-rule="evenodd" d="M19.916 4.626a.75.75 0 0 1 .208 1.04l-9 13.5a.75.75 0 0 1-1.154.114l-6-6a.75.75 0 0 1 1.06-1.06l5.353 5.353 8.493-12.74a.75.75 0 0 1 1.04-.207Z" clip-rule="evenodd" />
+                        </svg>
                     </button>
                     <button class="delete-btn" onclick="window.pomodoroTimer.deleteTask(${this.currentTask.id})">
-                        <i class="fas fa-times"></i>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="20" height="20">
+                            <path fill-rule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+                        </svg>
                     </button>
                 </div>
             </div>
